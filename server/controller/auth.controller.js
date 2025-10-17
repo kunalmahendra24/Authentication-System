34915c3b5e2385b5eea72a129a1fac11f2955c66
user.name : "kunal mahendra <user.email:"kunalmahendra2@gmail.com>
2025-10-18 02:04:34 +0530

    same site changes

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -36,7 +36,7 @@ export const register = async (req, res) => {
     res.cookie('token', token, { 
     httpOnly: true, 
     secure: true, 
-    sameSite: isProduction ? 'None' : 'Lax', 
+    sameSite: 'None', 
     maxAge: 7*24*60*60*1000 
     });
 
@@ -86,7 +86,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, { 
         httpOnly: true, 
         secure: true, 
-        sameSite: isProduction ? 'None' : 'Lax', 
+        sameSite: 'None', 
         maxAge: 7*24*60*60*1000 
     });
         res.status(200).json({ success:true, message: "Login successful", user, token });
