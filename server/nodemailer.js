ec814178a9455d4b35e2618f9a29627c31b36924
user.name : "kunal mahendra <user.email:"kunalmahendra2@gmail.com>
2025-10-18 12:54:41 +0530

    nodemailer changes done

--- server/nodemailer.js
+++ server/nodemailer.js
@@ -4,9 +4,13 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
     port: 587,
+    secure:false,
     auth: {     
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
+    },
+    tls:{
+        rejectUnauthorized: false,
     },
 });
 export default transporter;
