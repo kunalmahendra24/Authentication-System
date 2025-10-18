6fb6b65f4046c90649263d3c0d07b9f10d6075f5
kunalmahendra24 <159016854+kunalmahendra24@users.noreply.github.com>
2025-10-18 13:22:39 +0530

    Update nodemailer.js

--- server/nodemailer.js
+++ server/nodemailer.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
+    port: 2525,
     secure:false,
     auth: {     
         user: process.env.SMTP_USER,
